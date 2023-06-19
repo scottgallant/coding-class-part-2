f6ce64f25984c8f7ba5f0548f666b20ffb1bef7e
scottgallant <scottgallant@users.noreply.github.com>
2023-06-19 20:10:58 -0300

    2 files modified

--- scripts.js
+++ scripts.js
@@ -1,3 +1,18 @@
 function doSomethingCool() {
-  alert('WARNING!');
+  //  alert('WARNING!');
+
+  // var x = "warning via var";
+
+  // var x = Math.random() * 10;
+  // var y = Math.floor(x);
+  // alert(y);
+
+  // var name = document.querySelector('#name').value;
+  // alert(name + ' Is Cool');
+
+  var div = document.querySelector('.child');
+  var p = document.createElement('p');
+  var name = document.querySelector('#name').value;
+  p.textContent = name;
+  div.appendChild(p);
 }
