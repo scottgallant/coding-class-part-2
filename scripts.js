4962f0abf839632150e5d5dcff1f45725e2e7601
scottgallant <scottgallant@users.noreply.github.com>
2023-06-26 17:35:04 -0300

    2 files modified

--- scripts.js
+++ scripts.js
@@ -17,7 +17,15 @@ function doSomethingCool() {
 
   var div = document.querySelector('.child');
   var p = document.createElement('p');
-  var name = document.querySelector('#name').value;
-  p.textContent = name;
+  var adj = document.querySelector('#adj').value;
+  var noun = document.querySelector('#noun').value;
+  var noun2 = document.querySelector('#noun2').value;
+  p.textContent =
+    "It's important to be " +
+    adj +
+    " when you're out in the woods. You'll want to pack plenty of " +
+    noun +
+    ' incase you get hungry, along with your binoculars, a sketch book, and a bag for collecting plenty of ' +
+    noun2;
   div.appendChild(p);
 }
